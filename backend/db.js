172dd928837309114e27489f13c61375eded4132
Priansh-Dev <prianshprakash.dev@gmail.com>
2026-03-23 22:13:48 +0530

    force raw dateStrings to prevent IST timezone shifting bugs on Render

--- backend/db.js
+++ backend/db.js
@@ -1,19 +1,18 @@
 const mysql = require('mysql2/promise');
 
-const sslConfig = process.env.MYSQL_HOST && process.env.MYSQL_HOST.includes('aivencloud') 
-  ? { rejectUnauthorized: false } 
-  : undefined;
-
 const pool = mysql.createPool({
   host: process.env.MYSQL_HOST,
   port: Number(process.env.MYSQL_PORT || 3306),
   user: process.env.MYSQL_USER,
   password: process.env.MYSQL_PASSWORD,
   database: process.env.MYSQL_DATABASE,
+  dateStrings: true,
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
-  ssl: sslConfig
+  ssl: process.env.MYSQL_HOST && process.env.MYSQL_HOST.includes('aivencloud') 
+    ? { rejectUnauthorized: false } 
+    : undefined
 });
 
 module.exports = { pool };
