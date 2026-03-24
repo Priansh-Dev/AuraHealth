064fee180bd6569a9609c519ab8a86935cd96da5
Priansh-Dev <prianshprakash.dev@gmail.com>
2026-03-24 15:17:05 +0530

    add TURN server to guarantee remote video feed connects through production firewalls

--- public/teleconsult.js
+++ public/teleconsult.js
@@ -55,7 +55,25 @@ async function loadAppt() {
 }
 
 const rtcConfig = {
-  iceServers: [{ urls: 'stun:stun.l.google.com:19302' }]
+  iceServers: [
+    { urls: 'stun:stun.l.google.com:19302' },
+    // A free public TURN server to guarantee video connection across strict firewalls / mobile networks
+    {
+      urls: 'turn:openrelay.metered.ca:80',
+      username: 'openrelayproject',
+      credential: 'openrelayproject'
+    },
+    {
+      urls: 'turn:openrelay.metered.ca:443',
+      username: 'openrelayproject',
+      credential: 'openrelayproject'
+    },
+    {
+      urls: 'turn:openrelay.metered.ca:443?transport=tcp',
+      username: 'openrelayproject',
+      credential: 'openrelayproject'
+    }
+  ]
 };
 
 function setMsg(m) {
